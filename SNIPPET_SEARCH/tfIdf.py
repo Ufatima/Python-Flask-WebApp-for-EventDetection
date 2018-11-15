550f7249d90a5be31915c49b66bed336ba9d9a9c
UmmulFatima <ufatima@student.oulu.fi>
2018-11-15 21:57:50 +0200

    Update tfIdf.py

--- SNIPPET_SEARCH/tfIdf.py
+++ SNIPPET_SEARCH/tfIdf.py
@@ -63,12 +63,28 @@ class tf_idf(object):
             tfIdf_event.append(s[1])
         return tfIdf_event
 
-    def generate_tfIdf_frequency_array(self, doc_complete):
-        text_string = ', '.join(doc_complete)
-        fullwordlist = self.stripNonAlphaNum(text_string)
-        word_list = self.removeStopwords(fullwordlist, stopwords)
-        word_list = [w for w in word_list if w not in stopwords_list]
-        word_list = self.remove_punctuaions(word_list, pun)
-        dictionary = self.wordListToFreqDict(word_list)
+   def generate_tfIdf_frequency_array(self, doc_complete):
+        wordlist = []
+        for doc in doc_complete:
+            doc = doc.replace("#", "").replace("_", " ")  # Removing HASH symbol
+            doc = re.sub(r'[^\x00-\x7f]', r'', doc)  # Removing the Hex characters(emoji)
+            stop_free = " ".join([i for i in doc.lower().split() if i not in stopwords])
+            stop_free = " ".join([i for i in stop_free.lower().split() if i not in stopwords_list])
+            text = re.sub(r'[^a-zA-Z0-9@\S]', ' ', stop_free) # removing punctuations
+            remove_pun = str.maketrans({key: None for key in punctuation})
+            punc_free = text.translate(remove_pun)
+            f = punc_free.split()
+            wordlist.append(f)
+        fullwordlist = []
+        for item in wordlist:
+            if isinstance(item, (str, int, bool)):
+                fullwordlist.append(item)
+            elif isinstance(item, dict):
+                for i in item.items():
+                    fullwordlist.extend(i)
+            else:
+                fullwordlist.extend(list(item))
+        print(fullwordlist)
+        dictionary = self.wordListToFreqDict(fullwordlist)
         sortdict = self.sortFreqDict(dictionary)
         return sortdict
