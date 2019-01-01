ae8b45e71968b6b222a5e00b86321efe3461e0c7
ummulfatima <hoory1020@gmail.com>
2019-01-01 20:33:05 +0200

    added snippets null check in main

--- main.py
+++ main.py
@@ -19,27 +19,31 @@ def result():
     if request.method == 'POST':
         user_Input = request.form
         out_txt = looker.snippets(user_Input['term'], user_Input['quantity'] )
-        topic = looker.generate_topic(out_txt)
-        tf_Idf_topic = looker.generate_tfidf_topic(out_txt)
 
-        # generate wordcloud
-        looker.create_wordcloud()
-        PEOPLE_FOLDER = os.path.join('static', 'img')
-        app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
-        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'h.jpg')
+        if out_txt:
+            topic = looker.generate_topic(out_txt)
+            tf_Idf_topic = looker.generate_tfidf_topic(out_txt)
 
-        # generate bar chart
-        sortdict_term_frequency = looker.create_tf_idf_dic(out_txt)
-        bar_labels = []
-        bar_values = []
-        for s in sortdict_term_frequency[:8]:
-            bar_labels.append(s[1])
-            bar_values.append(s[0])
+            # generate wordcloud
+            looker.create_wordcloud()
+            PEOPLE_FOLDER = os.path.join('static', 'img')
+            app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'h.jpg')
 
-        return render_template("template.html", result=out_txt, lda_topics=topic, tf_Idf_topics=tf_Idf_topic,
-                               a=user_Input['term'],
-                               c=user_Input['quantity'], user_image=full_filename, max=17000,
-                               labels=bar_labels, values=bar_values)
+            # generate bar chart
+            sortdict_term_frequency = looker.create_tf_idf_dic(out_txt)
+            bar_labels = []
+            bar_values = []
+            for s in sortdict_term_frequency[:8]:
+                bar_labels.append(s[1])
+                bar_values.append(s[0])
+
+            return render_template("template.html", result=out_txt, lda_topics=topic, tf_Idf_topics=tf_Idf_topic,
+                                   a=user_Input['term'],
+                                   c=user_Input['quantity'], user_image=full_filename, max=17000,
+                                   labels=bar_labels, values=bar_values)
+        else:
+            return render_template("index.html")
 
 
 # No caching at all for API endpoints.
